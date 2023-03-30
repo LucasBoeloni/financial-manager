2e712a7daf1c5863fee729b03452946e0b6cd5e3
LucasBoeloni <lucasboeloni@gmail.com>
2023-03-30 11:28:29 -0300

    initial commit

---
+++ service/src/main/java/com/personal/service/SgfServiceApplication.java
@@ -0,0 +1,13 @@
+package com.personal.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgfServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgfServiceApplication.class, args);
+	}
+
+}
---
+++ service/src/test/java/com/personal/service/SgfServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgfServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
