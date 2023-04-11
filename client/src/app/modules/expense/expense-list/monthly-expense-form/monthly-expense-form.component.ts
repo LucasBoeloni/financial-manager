3102deadaa3985e61d40c9829486534936b9c78c
LucasBoeloni <lucasboeloni@gmail.com>
2023-04-11 14:26:36 -0300

    first version of monthly expense modal

--- client/src/app/modules/expense/expense-list/monthly-expense-form/monthly-expense-form.component.ts
+++ client/src/app/modules/expense/expense-list/monthly-expense-form/monthly-expense-form.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {ExpenseService} from "../../service/expense.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -7,14 +8,32 @@ import {ExpenseService} from "../../service/expense.service";
   templateUrl: './monthly-expense-form.component.html',
   styleUrls: ['./monthly-expense-form.component.scss']
 })
-export class MonthlyExpenseFormComponent {
+export class MonthlyExpenseFormComponent implements OnInit{
 
   @Input() visible: boolean = false;
   @Output() visibleChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+  form: FormGroup = new FormBuilder().group({});
+  formBuilder: FormBuilder = new FormBuilder();
+
   constructor(
     private service: ExpenseService
   ) {
+  }
+
+  days: number[] = Array.from({length: 25}, (_, i) => i + 1)
+
+  ngOnInit(): void {
+    this.buildEmptyForm()
+  }
+
+  buildEmptyForm(): void {
+    this.form = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      startDate: [null, [Validators.required]],
+      endDate: [null],
+      day: [null, [Validators.required]],
+    })
   }
 
   onClose() {
